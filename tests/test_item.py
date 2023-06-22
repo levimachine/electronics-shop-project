c72c55b55b412b0b99ce24b611ab912af48927d7
Евгений Евстратьев <levipupsik321@gmail.com>
2023-06-22 22:54:26 +0300

    загрузил модули pytest и pytest-cov покрытие тестами 100% конструктор реализовал, методы реализовал, файл main.py работает корректно

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item_calculate_total_price():
+    example_item = Item('Кабачок', 100, 5)
+    assert example_item.calculate_total_price() == 500
+
+
+def test_item_apply_discount():
+    example_item = Item('Кабачок', 100, 5)
+    Item.pay_rate = 2
+    example_item.apply_discount()
+    assert example_item.price == 200
